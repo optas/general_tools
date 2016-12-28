9ed2b010cf4bcd59955155d6824f3e17ef7ebdec
Panos <optas@stanford.edu>
2016-12-27 17:04:28 -0800

    shuffle lines of file

--- in_out/basics.py
+++ in_out/basics.py
@@ -22,3 +22,13 @@ def copy_folder_structure(top_dir, out_dir):
     all_dirs.next() # Exhaust first name which is identical to the top_dir.    
     for d in all_dirs:                
         create_dir(osp.join(out_dir, d.replace(top_dir, '')))
+        
+
+def shuffle_lines_of_file(in_file, out_file, seed=0):
+    np.random.seed(seed) 
+    with open(in_file, 'r') as f_in:
+        all_lines = f_in.readlines()
+        np.random.shuffle(all_lines)
+        
+    with open(out_file, 'w') as f_out:
+        f_out.writelines(all_lines)
