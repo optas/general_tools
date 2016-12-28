ea23157052d5d71154ffe2444ab7af63ffacf38c
Panos <optas@stanford.edu>
2016-12-27 16:24:17 -0800

    in_out

---
+++ in_out/basics.py
@@ -0,0 +1,24 @@
+'''
+Created on December 27, 2016
+
+@author:    Panos Achlioptas
+@contact:   pachlioptas @ gmail.com
+@copyright: You are free to use, change, or redistribute this code in any way you want for non-commercial purposes.
+'''
+
+import os
+import os.path as osp
+
+def create_dir(dir_path):
+    if not osp.exists(dir_path):
+        os.makedirs(dir_path)
+        
+                
+def copy_folder_structure(top_dir, out_dir):    
+    if top_dir[-1] != os.sep:
+        top_dir += os.sep
+    
+    all_dirs  = (dir_name for dir_name, _, _ in os.walk(top_dir))
+    all_dirs.next() # Exhaust first name which is identical to the top_dir.    
+    for d in all_dirs:                
+        create_dir(osp.join(out_dir, d.replace(top_dir, '')))
