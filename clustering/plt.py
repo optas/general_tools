dc099bb5e013511ddeee5044f1a7a943c1b39448
Panos <optas@stanford.edu>
2017-04-08 16:12:04 -0700

    grid-thumbnail

--- clustering/plt.py
+++ clustering/plt.py
@@ -7,6 +7,8 @@ Created on December 26, 2016
 import itertools
 import numpy as np
 import matplotlib.pylab as plt
+import cv2
+from PIL import Image
 
 
 def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
@@ -37,3 +39,110 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+
+
+def _scale_2d_embedding(two_dim_emb):
+    two_dim_emb -= np.min(two_dim_emb, axis=0)  # scale x-y in [0,1]
+    two_dim_emb /= np.max(two_dim_emb, axis=0)
+    return two_dim_emb
+
+
+def plot_2d_embedding_in_grid_greedy_way(two_dim_emb, image_files, big_dim=2500, small_dim=200, save_file=None):
+    '''
+    Input:
+        two_dim_emb: (N x 2) numpy array: arbitrary 2-D embedding of data.
+        image_files: (list) of strings pointing to images. Specifically image_files[i] should be an image associated with
+                     the datum whose coordinates are given in two_dim_emb[i].
+        big_dim:     (int) height of output 'big' grid rectangular image.
+        small_dim:   (int) height to which each individual rectangular image/thumbnail will be resized.
+    '''
+    ceil = np.ceil
+    mod = np.mod
+    x = _scale_2d_embedding(two_dim_emb)
+    out_image = np.zeros((big_dim, big_dim, 3), dtype='uint8')
+
+    for i, im_file in enumerate(image_files):
+        #  Determine location on grid
+        a = ceil(x[i, 0] * (big_dim - small_dim) + 1)
+        b = ceil(x[i, 1] * (big_dim - small_dim) + 1)
+        a = int(a - mod(a - 1, small_dim) + 1)
+        b = int(b - mod(b - 1, small_dim) + 1)
+
+        if out_image[a, b, 0] != 0:
+            continue    # Spot already filled.
+
+        fig = cv2.imread(im_file)
+        fig = cv2.resize(fig, (small_dim, small_dim))
+
+        try:
+            out_image[a:a + small_dim, b:b + small_dim, :] = fig
+        except:
+                print 'the code here fails. fix it.'
+                print a
+        continue
+
+    if save_file is not None:
+        im = Image.fromarray(out_image)
+        im.save(save_file)
+
+    return out_image
+
+
+def plot_2d_embedding_in_grid_forceful(two_dim_emb, image_files, big_dim=2500, small_dim=200, save_file=None):
+    x = _scale_2d_embedding(two_dim_emb)
+    out_image = np.zeros((big_dim, big_dim, 3), dtype='uint8')
+    N = two_dim_emb.shape[0]
+    xnum = int(big_dim / float(small_dim))
+    ynum = int(big_dim / float(small_dim))
+    used = np.zeros(N, dtype=np.bool)
+
+    if save_file is not None:
+        im = Image.fromarray(out_image)
+        im.save(save_file)
+    
+    return out_image 
+
+    # MATLAB CODE
+#     S = 2000; % size of final image
+#     G = zeros(S, S, 3, 'uint8');
+#     s = 50; % size of every image thumbnail
+#     
+#     xnum = S/s;
+#     ynum = S/s;
+#     used = false(N, 1);
+#     
+#     qq=length(1:s:S);
+#     abes = zeros(qq*2,2);
+#     i=1;
+#     for a=1:s:S
+#         for b=1:s:S
+#             abes(i,:) = [a,b];
+#             i=i+1;
+#         end
+#     end
+#     %abes = abes(randperm(size(abes,1)),:); % randperm
+#     
+#     for i=1:size(abes,1)
+#         a = abes(i,1);
+#         b = abes(i,2);
+#         %xf = ((a-1)/S - 0.5)/2 + 0.5; % zooming into middle a bit
+#         %yf = ((b-1)/S - 0.5)/2 + 0.5;
+#         xf = (a-1)/S;
+#         yf = (b-1)/S;
+#         dd = sum(bsxfun(@minus, x, [xf, yf]).^2,2);
+#         dd(used) = inf; % dont pick these
+#         [dv,di] = min(dd); % find nearest image
+#     
+#         used(di) = true; % mark as done
+#         I = imread(fs{di});
+#         if size(I,3)==1, I = cat(3,I,I,I); end
+#         I = imresize(I, [s, s]);
+#     
+#         G(a:a+s-1, b:b+s-1, :) = I;
+#     
+#         if mod(i,100)==0
+#             fprintf('%d/%d\n', i, size(abes,1));
+#         end
+#     end
+#     
+#     imshow(G);    
