f03cc0c861c340e03af85c31a2c12c9254604f7f
Panos <optas@stanford.edu>
2017-04-29 20:34:07 -0700

    mdb

--- simpletons.py
+++ simpletons.py
@@ -11,8 +11,9 @@ import numpy as np
 def sort_dict_by_key(in_dict, reverse=False):
     return sorted(in_dict.items(), key=operator.itemgetter(1), reverse=reverse)
 
+
 def select_first_last_and_k(in_list, k):
-    '''select the first and last element of a list among exactly k elements equally spaced
+    '''Select the first and last element of a list among exactly k elements equally spaced
     in the in_list[1:-1]
     '''
     f = in_list[0]
@@ -22,3 +23,20 @@ def select_first_last_and_k(in_list, k):
     res.insert(0, f)
     res.append(e)
     return res
+
+
+def indices_in_iterable(target, queries):
+    '''Find index of each item of the 'queries' in the 'target'.
+    '''
+    if len(np.unique(np.array(target, dtype=object))) != len(target):
+        raise ValueError('Target has to be comprised by unique elements.')
+
+    d = {name: i for i, name in enumerate(target)}
+    mapping = []
+    for name in queries:
+        if name not in d:
+            mapping.append(-1)
+        else:
+            mapping.append(d[name])
+    mapping = np.array(mapping)
+    return np.array(mapping)
