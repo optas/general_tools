accebee7dcdb347733c207d191edbbe87dc8b995
Panos <optas@stanford.edu>
2018-05-15 13:13:28 -0700

    more-mnemonics

--- notebook/utils.py
+++ notebook/utils.py
@@ -16,4 +16,5 @@ def mnemonics():
     s = '1. Start interactive matplotlib plots: %matplotlib nbagg\n'
     s += '2. Use plotly: from plotly.offline import init_notebook_mode\n init_notebook_mode(connected=True)\n'
     s += '3. Turn off plotting. plt.ioff() and plt.close() in for-loop.'
+    s += '4. Debugger set brkpoint: import ipdb; ipdb.set_trace()'
     print s
