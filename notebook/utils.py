dc02f56a6bdb6f14f43eebaf98082a08f1b5eca5
Panos Achlioptas <optas@stanford.edu>
2018-08-10 12:52:10 -0700

    mdb

--- notebook/utils.py
+++ notebook/utils.py
@@ -4,7 +4,7 @@ Created on Apr 27, 2017
 @author: optas
 '''
 import subprocess
-
+import warnings
 
 def run_command(cmd):
     """Run command, return output as string."""
@@ -12,10 +12,39 @@ def run_command(cmd):
     return output.decode("ascii")
 
 
+def grep_files(pattern, top_dir='./', match_whole_word=True, options=None):
+    def enclose_with_quotes(var):
+        return '\'' + var + '\''
+    
+    cmd = 'grep '
+    if options is not None:
+        cmd += options
+    
+    cmd += ' -rn' # r: recursice, n: print line number
+
+    if match_whole_word:
+        cmd += 'w' 
+
+    cmd += ' ' + enclose_with_quotes(top_dir)
+    cmd += ' -e ' + enclose_with_quotes(pattern)
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    if err is not None:
+        warnings.warn(err) 
+        
+    print out
+    
+    
 def mnemonics():
-    s = '1. Start interactive matplotlib plots: %matplotlib nbagg\n'
-    s += '2. Use plotly: from plotly.offline import init_notebook_mode\n init_notebook_mode(connected=True)\n'
-    s += '3. Turn off plotting. plt.ioff() and plt.close() in for-loop.'
-    s += '4. Debugger set brkpoint: import ipdb; ipdb.set_trace()'
-    s += '5. Reset jupyter\'s signature database: cd $(jupyter --data-dir); rm nbsignatures.db'
-    print s
+    rules = [
+    'Start interactive matplotlib plots: %matplotlib nbagg',
+    'Use plotly: from plotly.offline import init_notebook_mode; init_notebook_mode(connected=True)',
+    'Many figure plot/saving. Turn off interactive plotting.\nplt.ioff() and plt.close() in for-loop.',
+    'Debugger set brkpoint: import ipdb; ipdb.set_trace()',
+    'Reset jupyter\'s signature database: cd $(jupyter --data-dir); rm nbsignatures.db',
+    'Plot multiple images inside single cell.\nfrom IPython.display import display; display(Fig1), display(Fig2)'        
+    ]
+    for i, r in enumerate(rules):
+        print str(i) + '.\t', r + '\n'
