38ca7afa17fb3cf2940514f2bcaa3ff4486805a1
Panos <optas@stanford.edu>
2018-11-17 23:20:25 -0800

    mdb

---
+++ pil_wrapper.py
@@ -0,0 +1,29 @@
+'''
+Created on November 17, 2018
+
+@author: optas
+'''
+
+from PIL import Image
+
+
+def square_image(img_file, desired_size, im_type='RGB'):
+    ''' Will load and resize the image to a square one, while keeping the original
+    aspect ratio and add padding if necessary to achieve this.
+    Input:
+        im_type: (string) PIL compatible image-type, e.e.g, RGBA, LA, L.
+    '''
+    try:
+        image = Image.open(img_file)
+    except IOError:
+            print "Cannot open image-file '%s'" % img_file
+            
+    image.thumbnail((desired_size, desired_size), Image.ANTIALIAS)
+
+    new_size = image.size
+    new_im = Image.new(im_type, (desired_size, desired_size))
+    new_im.paste(image, ((desired_size - new_size[0]) // 2, 
+                         (desired_size - new_size[1]) // 2))
+
+    assert(new_im.size == (desired_size, desired_size))
+    return new_im
